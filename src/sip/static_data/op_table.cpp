9476d51be55282f745e0ca4939937a3edb160783
Beverly Sanders <sanders@cise.ufl.edu>
2014-06-23 22:23:11 -0400

    Extracted the enum defining opcodes into separate files.
    This refactoring is in preparation for automatic generation of
    the .h file from the corresponding definition in the SIAL compiler.

--- src/sip/static_data/op_table.cpp
+++ src/sip/static_data/op_table.cpp
@@ -6,35 +6,8 @@
  */
 
 #include "op_table.h"
-#include <stdexcept>
 
-namespace sip {
-
-opcode_t intToOpcode(int opcode) {
-	if (101 <= opcode && opcode < last_opcode) {
-		return (opcode_t) opcode;
-	}
-	throw std::domain_error("illegal opcode value");
-}
-
-std::string opcodeToName(opcode_t op){
-	switch(op){
-#define SIPOP(e,n,t,p) case e: return std::string(t);
-	SIP_OPCODES
-#undef SIPOP
-	}
-	sip::fail("Interal error ! opcode not recognized !");
-	return std::string("");
-}
-
-bool printableOpcode(opcode_t op){
-	switch(op){
-#define SIPOP(e,n,t,p) case e: return p;
-	SIP_OPCODES
-#undef SIPOP
-	}
-	return false;
-}
+namespace sip{
 
 OpTableEntry::OpTableEntry() {}
 
