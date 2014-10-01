d77895b285afeeed2ce511b97a8c1ca85b8e8d04
Jason Byrd <jasonnbyrd@gmail.com>
2014-10-01 14:58:25 -0400

    debug print removed from broadcast_static

--- src/sip/worker/interpreter.cpp
+++ src/sip/worker/interpreter.cpp
@@ -183,7 +183,7 @@ void Interpreter::interpret(int pc_start, int pc_end) {
 		}
 			break;
 		case broadcast_static_op: {
-			std::cout << "calling broadcast_static with args " << arg0() << ", " << control_stack_.top() << std::endl;
+//			std::cout << "calling broadcast_static with args " << arg0() << ", " << control_stack_.top() << std::endl;
 			Block::BlockPtr block = get_static(arg0());
 			sial_ops_.broadcast_static(block, control_stack_.top());
 			control_stack_.pop();
