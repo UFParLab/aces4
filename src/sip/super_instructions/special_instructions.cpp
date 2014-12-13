ce3beb5dfdbcd97f948e815ca5ecfa9673dba10e
Nakul Jindal <nakul02@ufl.edu>
2014-12-12 22:38:01 -0500

    Added super instruction to dump block to file

--- src/sip/super_instructions/special_instructions.cpp
+++ src/sip/super_instructions/special_instructions.cpp
@@ -213,6 +213,7 @@ void energy_ty_denominator_rhf(
  //the extern C block)
 void print_block(int& array_slot, int& rank, int* index_values, int& size, int* extents,  double* data, int& ierr);
 //void test_print_block(int& array_slot, int& rank, int* index_values, int& size, int* extents,  double* data, int& ierr);
+void write_block_to_file(int& array_slot, int& rank, int* index_values, int& size, int* extents,  double* data, int& ierr);
 void print_static_array(int& array_slot, int& rank, int* index_values, int& size, int* extents, double* data, int& ierr);
 void get_my_rank(int& array_slot, int& rank, int* index_values, int& size, int* extents, double* data, int& ierr);
 void list_block_map();
@@ -379,6 +380,7 @@ void SpecialInstructionManager::init_procmap(){
 //	procmap_["test_print_block"]=(fp0)&test_print_block;
 
 	procmap_["print_block"]=(fp0)&print_block;
+	procmap_["write_block_to_file"]=(fp0)&write_block_to_file;
 	procmap_["print_static_array"]=(fp0)&print_static_array;
 	procmap_["list_block_map"]=(fp0)&list_block_map;
 	procmap_["compute_aabb_batch"]=(fp0)&compute_aabb_batch;
