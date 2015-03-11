3d907c1d63c95b06d19002ace8c2d2e5ef5434b9
Jason Byrd <jasonnbyrd@gmail.com>
2015-03-11 11:48:31 -0400

    minor tweak to reader.
    accounting for arbitrary index starts due to QM offsets
    since files end with \n, a -- to computed length was needed.

--- src/sip/super_instructions/general/read_block_from_text_file.cpp
+++ src/sip/super_instructions/general/read_block_from_text_file.cpp
@@ -32,26 +32,43 @@ void read_block_from_text_file(int& array_slot, int& rank, int* index_values, in
 	std::cout << "Reading contents of block " << block_name_ss.str() << " from file " << file_ss.str() << std::endl;
 
 	int *indices = new int[rank];
+	int *offset = new int[rank];
 
 	long read_size = 0;
 	while (!input_file.eof()) {
 		for (int i=0; i<rank; ++i){
 			input_file >> indices[i];
 		}
+/*
+    Fortran generated files are 1 based, additionally when 
+    reading in virtual subset indices we are not 1 based at all but occupied+1 based.  
+    Since we assume the whole array is read in from the file, it is possible to arbitrarily
+    take into account the starting index by grabbing the first line of the file and storing 
+    that set of index values as the offset.  
+*/
+		if (read_size==0){
+		for (int i=0; i<rank; ++i){
+			offset[i] = indices[i];
+		}
+		}
 		double val;
 		input_file >> val;
 		read_size++;
 
+// rolling indices based on fortran vector storage.
 		int loc = indices[rank-1] - 1;
 		for (int i=rank-2; i>=0; --i){
 			loc *= extents[i];
-			loc += indices[i] - 1;
+			loc += indices[i] - offset[i];
 		}
 
-		sip::check (loc != -1, "Invalid index for block " + block_name_ss.str());
+		//sip::check (loc != -1, "Invalid index for block " + block_name_ss.str());
 		data[loc] = val;
 
+//        std::cout << read_size << " "  << size << std::endl;
 	}
+// files have new line at the end usually....
+	read_size--;
 
 	sip::check(read_size == size, "Incorrect number of elements in file " + file_ss.str());
 
