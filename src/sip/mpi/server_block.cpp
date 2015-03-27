99026fc3c03672291e1961cd88cd8fde2242bd72
Nakul Jindal <nakul02@ufl.edu>
2015-03-27 13:02:58 -0400

    Moved block consistency semantics in ServerBlock to separate class.

--- src/sip/mpi/server_block.cpp
+++ src/sip/mpi/server_block.cpp
@@ -21,8 +21,7 @@ namespace sip {
 const std::size_t ServerBlock::field_members_size_ = sizeof(int) + sizeof(int) + sizeof(dataPtr);
 std::size_t ServerBlock::allocated_bytes_ = 0;
 
-ServerBlock::ServerBlock(int size, bool initialize):
-		size_(size), consistency_status_(std::make_pair(NONE, OPEN)){
+ServerBlock::ServerBlock(int size, bool initialize): size_(size) {
 	if (initialize){
 		data_ = new double[size_]();
     } else {
@@ -39,8 +38,7 @@ ServerBlock::ServerBlock(int size, bool initialize):
 	allocated_bytes_ += size_ * sizeof(double);
 }
 
-ServerBlock::ServerBlock(int size, dataPtr data):
-		size_(size), data_(data), consistency_status_(std::make_pair(NONE, OPEN)) {
+ServerBlock::ServerBlock(int size, dataPtr data): size_(size), data_(data){
 	disk_status_[ServerBlock::IN_MEMORY] = (data_ == NULL) ? false : true;
 	disk_status_[ServerBlock::ON_DISK] = false;
 	disk_status_[ServerBlock::DIRTY_IN_MEMORY] = false;
@@ -128,155 +126,6 @@ std::ostream& operator<<(std::ostream& os, const ServerBlock& block) {
 	return os;
 }
 
-void ServerBlock::reset_consistency_status (){
-	CHECK(consistency_status_.first != INVALID_MODE &&
-			consistency_status_.second != INVALID_WORKER,
-			"Inconsistent block status !");
-	consistency_status_.first = NONE;
-	consistency_status_.second = OPEN;
-}
-
-
-bool ServerBlock::update_and_check_consistency(SIPMPIConstants::MessageType_t operation, int worker){
-	/**
-	 * Block Consistency Rules
-	 *
-	 *     		  GET,w    PUT,w    PUT_ACC,w   GET,w1     PUT,w1  PUT_ACC,w1
-	 *		NO      Rw      Ww         Aw         Rw1        Ww1     Aw1
-	 *		Rw      Rw      Sw         Sw         RM          X       X
-	 *		RM      RM       X          X          RM         X       X
-	 *		Ww      Sw      Sw         Sw          X          X       X
-	 *		Aw      Sw      Sw         Aw          X          X       AM
-	 *		AM       X       X         AM          X          X       AM
-	 *		Sw      Sw      Sw         Sw          X          X       X
-	 */
-
-	ServerBlockMode mode = consistency_status_.first;
-	int prev_worker = consistency_status_.second;
-
-	// Check if block already in inconsistent state.
-	if (mode == INVALID_MODE || prev_worker == INVALID_WORKER)
-		return false;
-
-
-	ServerBlockMode new_mode = INVALID_MODE;
-	int new_worker = INVALID_WORKER;
-
-	switch (mode){
-	case NONE: {
-		/*  		  GET,w    PUT,w    PUT_ACC,w   GET,w1     PUT,w1  PUT_ACC,w1
-		 *		NO      Rw      Ww         Aw         Rw1        Ww1     Aw1
-		 */
-		if (OPEN == prev_worker){
-			switch(operation){
-			case SIPMPIConstants::GET : 			new_mode = READ; 		new_worker = worker; break;
-			case SIPMPIConstants::PUT : 			new_mode = WRITE; 		new_worker = worker; break;
-			case SIPMPIConstants::PUT_ACCUMULATE :	new_mode = ACCUMULATE; 	new_worker = worker; break;
-			default : goto consistency_error;
-			}
-		} else {
-			goto consistency_error;
-		}
-	}
-		break;
-	case READ: {
-		/*  		  GET,w    PUT,w    PUT_ACC,w   GET,w1     PUT,w1  PUT_ACC,w1
-		 *		Rw      Rw      Sw         Sw         RM          X       X
-		 *		RM      RM       X          X          RM         X       X
-		 */
-		if (OPEN == prev_worker){
-			goto consistency_error;
-		} else if (MULTIPLE_WORKER == prev_worker){
-			switch(operation){
-			case SIPMPIConstants::GET :				new_mode = READ; new_worker = MULTIPLE_WORKER; break;
-			case SIPMPIConstants::PUT : case SIPMPIConstants::PUT_ACCUMULATE : {	goto consistency_error; } break;
-			default : goto consistency_error;
-			}
-		} else { 	// Single worker
-			switch(operation){
-			case SIPMPIConstants::GET :	new_mode = READ; new_worker = (worker == prev_worker ? worker : MULTIPLE_WORKER); break;
-			case SIPMPIConstants::PUT : case SIPMPIConstants::PUT_ACCUMULATE :{
-				if (worker != prev_worker)
-					goto consistency_error;
-				new_mode = SINGLE_WORKER;
-				new_worker = worker;
-			}
-			break;
-			default : goto consistency_error;
-			}
-		}
-	}
-		break;
-	case WRITE:{
-		/*  		  GET,w    PUT,w    PUT_ACC,w   GET,w1     PUT,w1  PUT_ACC,w1
-		 *		Ww      Sw      Sw         Sw          X          X       X
-		 */
-		if (prev_worker == worker){
-			new_mode = SINGLE_WORKER; new_worker = worker;
-		} else {
-			goto consistency_error;
-		}
-	}
-	break;
-	case ACCUMULATE:{
-		/*  		  GET,w    PUT,w    PUT_ACC,w   GET,w1     PUT,w1  PUT_ACC,w1
-		 *		Aw      Sw      Sw         Aw          X          X       AM
-		 *		AM       X       X         AM          X          X       AM
-		 */
-		if (OPEN == prev_worker){
-			goto consistency_error;
-		} else if (MULTIPLE_WORKER == prev_worker){
-			if (SIPMPIConstants::PUT_ACCUMULATE == operation){
-				new_mode = ACCUMULATE; new_worker = MULTIPLE_WORKER;
-			} else {
-				goto consistency_error;
-			}
-		} else { // Single worker
-			switch(operation){
-			case SIPMPIConstants::GET : case SIPMPIConstants::PUT : {
-				if (prev_worker != worker)
-					goto consistency_error;
-				new_worker = worker;
-				new_mode = SINGLE_WORKER;
-			}
-			break;
-			case SIPMPIConstants::PUT_ACCUMULATE :	{
-				new_mode = ACCUMULATE;
-				new_worker = (worker == prev_worker ? worker : MULTIPLE_WORKER);
-			}
-			break;
-			default : goto consistency_error;
-			}
-		}
-	}
-	break;
-	case SINGLE_WORKER:{
-		/*  		  GET,w    PUT,w    PUT_ACC,w   GET,w1     PUT,w1  PUT_ACC,w1
-		 *		Sw      Sw      Sw         Sw          X          X       X
-		 */
-		if (worker == prev_worker){
-			new_mode = SINGLE_WORKER; new_worker = worker;
-		} else {
-			goto consistency_error;
-		}
-	}
-	break;
-	default:
-		goto consistency_error;
-	}
-
-	consistency_status_.first = new_mode;
-	consistency_status_.second = new_worker;
-	return true;
-
-consistency_error:
-	SIP_LOG(std::cout << "Inconsistent block at server ")
-	consistency_status_.first = INVALID_MODE;
-	consistency_status_.second = INVALID_WORKER;
-
-	return false;
-
-}
 
 std::size_t ServerBlock::allocated_bytes(){
 	return allocated_bytes_;
--- src/sip/mpi/server_block.h
+++ src/sip/mpi/server_block.h
@@ -13,6 +13,7 @@
 #include "id_block_map.h"
 #include "sip_mpi_constants.h"
 #include "mpi_state.h"
+#include "distributed_block_consistency.h"
 
 namespace sip {
 
@@ -121,12 +122,14 @@ public:
 	 * @param worker
 	 * @return false if brought into inconsistent state by operation
 	 */
-	bool update_and_check_consistency(SIPMPIConstants::MessageType_t operation, int worker);
+	bool update_and_check_consistency(SIPMPIConstants::MessageType_t operation, int worker){
+		return block_consistency_.update_and_check_consistency(operation, worker);
+	}
 
 	/**
 	 * Resets consistency status of block to (NONE, OPEN).
 	 */
-	void reset_consistency_status ();
+	void reset_consistency_status () { block_consistency_.reset_consistency_status(); }
 
 	dataPtr get_data() { return data_; }
 	void set_data(dataPtr data) { data_ = data; }
@@ -149,6 +152,8 @@ private:
     const int size_;/**< Number of elements in block */
 	dataPtr data_;	/**< Pointer to block of data */
     MPIState state_;/**< For blocks busy in async MPI communication */
+    DistributedBlockConsistency block_consistency_;/**< To check that Blocks in GET, PUT, PUT+ are following correct semantics */
+
 
 	enum ServerBlockStatus {
 		IN_MEMORY		= 0,	// Block is on host
@@ -157,53 +162,6 @@ private:
 	};
 	std::bitset<3> disk_status_;
 
-
-	/**	 Structures and methods to check for block consistency during GET, PUT, PUT+ operations
-	 * from different workers in a pardo section
-	 */
-
-	enum ServerBlockMode {
-		NONE = 2,			// Block not being worked on
-		READ = 3, 			// GET/REQUEST done on block
-		WRITE = 4, 			// PUT/PREPARE done on block
-		ACCUMULATE = 5, 	// PUT+/PREPARE+ done on block
-		SINGLE_WORKER = 6,	// PUT/PUT+ and GET done by same worker
-		INVALID_MODE = 999
-	};
-
-	/** Can convert OPEN to ONE_WORKER and ONE_WORKER to MULTIPLE_WORKER */
-	enum ServerBlockWorker {
-		OPEN = -3,				// Block not worked on
-		ONE_WORKER = -2,		// Just one worker worked on block, UNUSED - actual rank is stored
-		MULTIPLE_WORKER = -1,	// More than one worker worked on block
-		INVALID_WORKER = -999
-	};
-
-	/**
-	 * The set of consistent states that a block can be in are shown.
-	 * Each row denote a starting state. A state is shown as <ServerBlockMode><ServerBlockWorker>
-	 * w is an arbitrary rank. w1 is another arbitrary rank not equal to w1.
-	 * Each column is an action on a block, the state of the block
-	 * is changed to that shown in the row.
-	 * In the table, for ServerBlockMode
-	 * N = NONE, R = READ, W = WRITE, A = ACCUMULATE, S = SINGLE_WORKER
-	 * For ServerBlockWorker
-	 * O = OPEN, w = worker rank (ONE_WORKER),  w1 = some other worker, M = MULTIPLE_WORKER
-	 *
-	 * The positions denoted with 'X' are error conditions.
-	 *
-	 *
-	 *    		  GET,w    PUT,w    PUT_ACC,w   GET,w1     PUT,w1  PUT_ACC,w1
-	 *		NO      Rw      Ww         Aw         Rw1        Ww1     Aw1
-	 *		Rw      Rw      Sw         Sw         RM          X       X
-	 *		RM      RM       X          X          RM         X       X
-	 *		Ww      Sw      Sw         Sw          X          X       X
-	 *		Aw      Sw      Sw         Aw          X          X       AM
-	 *		AM       X       X         AM          X          X       AM
-	 *		Sw      Sw      Sw         Sw          X          X       X
-	 */
-
-	std::pair<ServerBlockMode, int> consistency_status_; /*! State of block */
 
 	const static std::size_t field_members_size_;
 	static std::size_t allocated_bytes_;
