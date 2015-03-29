64dcf14677796fe06c5e3ceb1b412eec8637dc4b
Nakul Jindal <nakul02@ufl.edu>
2015-03-29 03:39:24 -0400

    Bug fixes to block consistency map interpreter.

--- src/sip/worker/block_consistency_interpreter.h
+++ src/sip/worker/block_consistency_interpreter.h
@@ -33,11 +33,34 @@ public:
 	virtual ~BlockConsistencyInterpreter();
 
 
-	virtual void handle_broadcast_static_op(int pc) { fail("handle_broadcast_static not modeled!", line_number()); }
+	void blk_instr() {
+		get_block_id_from_instruction(pc_);
+//		int array_id = arg1(pc_);
+//		if (sip_tables_.is_contiguous_local(array_id)){
+//			int rank = sip_tables_.array_rank(array_id);
+//			for (int j = rank-1; j >=0; --j){
+//				control_stack_.pop();
+//				control_stack_.pop();
+//			}
+//		}
+	}
+
+	void blk_sstack(){
+		get_block_id_from_selector_stack();
+//		BlockSelector selector = block_selector_stack_.top();
+//		block_selector_stack_.pop();
+//		int rank = sip_tables_.array_rank(selector.array_id_);
+//		for (int j = rank-1; j >=0; --j){
+//			control_stack_.pop();
+//			control_stack_.pop();
+//		}
+	}
+
+	virtual void handle_broadcast_static_op(int pc) { 	control_stack_.pop(); }
 	virtual void handle_allocate_op(int pc) { /* TODO Time to zero out blocks */ }
 	virtual void handle_deallocate_op(int pc) {}
-	virtual void handle_allocate_contiguous_op(int pc) { fail ("handle_allocate_contiguous_op not modeled!", line_number()); }
-	virtual void handle_deallocate_contiguous_op(int pc) { fail ("handle_deallocate_contiguous_op not modeled!", line_number()); }
+	virtual void handle_allocate_contiguous_op(int pc) { get_block_id_from_instruction(pc);}
+	virtual void handle_deallocate_contiguous_op(int pc) { get_block_id_from_instruction(pc);}
 
 	virtual void handle_collective_sum_op(int pc) {  expression_stack_.pop(); /* TODO Time for MPI_BCast */ }
 	virtual void handle_assert_same_op(int pc) { /* TODO Time for MPI_BCast */ }
@@ -45,7 +68,7 @@ public:
 	virtual void handle_print_scalar_op(int pc) {  expression_stack_.pop();}
 	virtual void handle_print_int_op(int pc) { control_stack_.pop();}
 	virtual void handle_print_index_op(int pc) { control_stack_.pop();}
-	virtual void handle_print_block_op(int pc) { block_selector_stack_.pop(); }
+	virtual void handle_print_block_op(int pc) { blk_sstack(); }
 	virtual void handle_println_op(int pc) {}
 	virtual void handle_gpu_on_op(int pc) {}
 	virtual void handle_gpu_off_op(int pc) {}
@@ -53,21 +76,24 @@ public:
 	virtual void handle_restore_persistent_op(int pc) {}
 	virtual void handle_create_op(int pc) {}
 
-	virtual void handle_block_copy_op(int pc) 		{ block_selector_stack_.pop(); }
-	virtual void handle_block_permute_op(int pc) 	{ block_selector_stack_.pop(); block_selector_stack_.pop();}
-	virtual void handle_block_fill_op(int pc) 		{ expression_stack_.pop(); }
-	virtual void handle_block_scale_op(int pc) 		{ expression_stack_.pop(); }
-	virtual void handle_block_scale_assign_op(int pc){ block_selector_stack_.pop(); expression_stack_.pop();}
-	virtual void handle_block_accumulate_scalar_op(int pc){ expression_stack_.pop(); }
-	virtual void handle_block_add_op(int pc)		{ block_selector_stack_.pop(); block_selector_stack_.pop();}
-	virtual void handle_block_subtract_op(int pc)	{ block_selector_stack_.pop(); block_selector_stack_.pop();}
-	virtual void handle_block_contract_op(int pc)	{ block_selector_stack_.pop(); block_selector_stack_.pop();}
-	virtual void handle_block_contract_to_scalar_op(int pc) { block_selector_stack_.pop(); block_selector_stack_.pop(); expression_stack_.push(0.0);}
-	virtual void handle_block_load_scalar_op(int pc){ block_selector_stack_.pop(); expression_stack_.push(0.0);}
+	virtual void handle_block_copy_op(int pc) 		{ blk_instr(); blk_sstack(); }
+	virtual void handle_block_permute_op(int pc) 	{ blk_sstack(); blk_sstack();}
+	virtual void handle_block_fill_op(int pc) 		{ blk_instr(); expression_stack_.pop(); }
+	virtual void handle_block_scale_op(int pc) 		{ blk_instr(); expression_stack_.pop(); }
+	virtual void handle_block_scale_assign_op(int pc){ blk_instr(); blk_sstack(); expression_stack_.pop();}
+	virtual void handle_block_accumulate_scalar_op(int pc){ blk_instr(); expression_stack_.pop(); }
+	virtual void handle_block_add_op(int pc)		{ blk_instr(); blk_sstack(); blk_sstack();}
+	virtual void handle_block_subtract_op(int pc)	{ blk_instr(); blk_sstack(); blk_sstack();}
+	virtual void handle_block_contract_op(int pc)	{ blk_instr(); blk_sstack(); blk_sstack();}
+	virtual void handle_block_contract_to_scalar_op(int pc) { blk_sstack(); blk_sstack(); expression_stack_.push(0.0);}
+	virtual void handle_block_load_scalar_op(int pc){ blk_sstack(); expression_stack_.push(0.0);}
 	virtual void handle_execute_op(int pc);
 
-	// Methods to override to figure out if blocks are doing the correct thing.
+	// Scalar methods to skip
+	virtual void handle_scalar_exp_op(int pc) { expression_stack_.pop(); expression_stack_.pop(); expression_stack_.push(0.0); }
+	virtual void handle_scalar_divide_op(int pc) { expression_stack_.pop(); expression_stack_.pop(); expression_stack_.push(0.0);}
 
+	// Methods to override to figure out if blocks are doing the correct thing.
 	virtual void handle_pardo_op(int &pc);	//! Overridden to simulate 1 worker at a time
 	virtual void handle_sip_barrier_op(int pc);
 	virtual void handle_program_end(int &pc);
