76b0d363b0c959ade544c54a4d26a9c04e1a9756
Ajith Perera <perera.ajith@gmail.com>
2015-11-15 10:23:47 -0500

    Initial commit of after getting the second derivative SIPs. They all reproduce ACES III checksum. Note that ACES III has done NAI integrals Hessian numerically (unknown reason). All the integral deriavative, Hessian SIps need to be reassed after consulting with Nobert.

--- src/sip/super_instructions/special_instructions.cpp
+++ src/sip/super_instructions/special_instructions.cpp
@@ -344,6 +344,24 @@ void return_2el_gradient(
         int& array_slot_3, int& rank_3, int * index_values_3, int& size_3, int * extents_3, double * data_3,
         int& ierr);
 
+void return_kin_ovl_hessints(
+        int& array_slot_0, int& rank_0, int * index_values_0, int& size_0, int * extents_0, double * data_0,
+        int& array_slot_1, int& rank_1, int * index_values_1, int& size_1, int * extents_1, double * data_1,
+        int& array_slot_2, int& rank_2, int * index_values_2, int& size_2, int * extents_2, double * data_2,
+        int& array_slot_3, int& rank_3, int * index_values_3, int& size_3, int * extents_3, double * data_3,
+	int& ierr);
+
+void return_nai_hessints(
+        int& array_slot_0, int& rank_0, int * index_values_0, int& size_0, int * extents_0, double * data_0,
+        int& array_slot_1, int& rank_1, int * index_values_1, int& size_1, int * extents_1, double * data_1,
+        int& array_slot_2, int& rank_2, int * index_values_2, int& size_2, int * extents_2, double * data_2,
+	int& ierr);
+
+void drive_2el_hessian(
+        int& array_slot_0, int& rank_0, int * index_values_0, int& size_0, int * extents_0, double * data_0,
+        int& array_slot_1, int& rank_1, int * index_values_1, int& size_1, int * extents_1, double * data_1,
+	int& ierr);
+
 void compute_nn_repulsion_grad(
         int& array_slot_1, int& rank_1, int * index_values_1, int& size_1, int * extents_1, double * data_1, 
 	int& ierr);
@@ -602,6 +620,9 @@ void SpecialInstructionManager::init_procmap(){
     procmap_["return_1el_grdints"]=(fp0)&return_1el_grdints;
     procmap_["return_2el_grdints"]=(fp0)&return_2el_grdints;
     procmap_["return_2el_gradient"]=(fp0)&return_2el_gradient;
+    procmap_["return_nai_hessints"]=(fp0)&return_nai_hessints;
+    procmap_["return_kin_ovl_hessints"]=(fp0)&return_kin_ovl_hessints;
+    procmap_["drive_2el_hessian"]=(fp0)&drive_2el_hessian;
     procmap_["print_block_and_index"]=(fp0)&print_block_and_index;
     procmap_["compute_nn_repulsion_grad"]=(fp0)&compute_nn_repulsion_grad;
 
